341da058c7f188e3fae660212a0a2308fde66444
Викусик <oblinnika@gmail.com>
2025-07-06 15:05:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ def diff_hell():
     print("= Протокол Диффи-Хеллмана =")
     # заглушка
 
+def rsa():
+    print("=== RSA Алгоритм ===")
+
+    p = int(input("Введите простое число p: "))
+    q = int(input("Введите простое число q: "))
+
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
+    print("n =", n)
+    print("phi =", phi)
+
 def main():
     print("Выберите алгоритм:")
     print("1 - RSA")
