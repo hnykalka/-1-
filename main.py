2a6a532b6685e0025501742ba8be7745e30ba128
Викусик <oblinnika@gmail.com>
2025-07-06 15:21:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
 def rsa():
-    print("= RSA Алгоритм =")
-    # заглушка
+    print("=== RSA Алгоритм ===")
+
+    p = int(input("Введите простое число p: "))
+    q = int(input("Введите простое число q: "))
+
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
+    print("n =", n)
+    print("phi =", phi)
+
+    while True:
+        e = int(input("Введите e (взаимно просто с phi): "))
+        if gcd(e, phi) == 1:
+            break
+        print("e не взаимно просто с phi! Попробуйте другое.")
+
+    print("Открытый ключ (e, n):", (e, n))
 
 def diff_hell():
     print("= Протокол Диффи-Хеллмана =")
